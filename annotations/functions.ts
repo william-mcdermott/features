9de825b7e82b8978e9f2f305ade561f0c84cbec6
William McDermott <williammcdermott@MacBookPro.lan>
2025-03-13 20:01:58 -0500

    fix error missing arrow

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -2,7 +2,7 @@ const add = (a: number, b: number): number => {
     return a + b;
 }
 
-const subtract = (a: number, b: number): number {
+const subtract = (a: number, b: number): number => {
     return a - b;
 }
 
