456cc70b35e154a14705e8bdbaa2454715e3ad5c
William McDermott <williammcdermott@MacBookPro.lan>
2025-03-18 23:34:07 -0500

    Type inference in arrays

--- arrays.ts
+++ arrays.ts
@@ -6,3 +6,15 @@ const carsByMake = [
     ['corolla'],
     ['camaro']
 ]
+
+// Help with inference when extracting values
+const carName = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+    return car.toUpperCase();
+})
